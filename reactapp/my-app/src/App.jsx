f5260e2d2ab7a201200e75962a36d452ce14eed7
jahanvi <jahanvi424@gmail.com>
2026-06-17 13:55:37 +0530

    Initial commit

---
+++ reactapp/my-app/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1> jahanvi is great very great</h1>
+      <h2> my name is raj kumar rana </h2>
+      <Props></Props>
+    </>
+  )
+}
+
+export default App
